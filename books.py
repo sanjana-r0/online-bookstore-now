2268dae2e087dbfac9d940f944163b085e60db82
sanjana-r0 <230701285@rajalakshmi.edu.in>
2026-09-02 18:25:50 +0530

    added books code

---
+++ books.py
@@ -0,0 +1,10 @@
+books = { 101: {"book1", "auth1", 2020}, 102: {"book2", "auth2", 2021}}
+
+#add books
+
+book_id=103
+book_name = "book3"
+book_author = "auth2"
+book_year = 2023
+
+books[book_id] = {book_name, book_author, book_year}
